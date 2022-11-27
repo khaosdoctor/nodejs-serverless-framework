14b6e3fd0db3d04293bcb88da2e84a2d21d61de6
Lucas Santos <lhs.santoss@gmail.com>
2022-11-27 16:44:29 +0100

    feat(basic): add basic auth

--- api/handlers.js
+++ api/handlers.js
@@ -1,7 +1,10 @@
 'use strict'
+const { pbkdf2Sync } = require('crypto')
 const { MongoClient, ObjectId } = require('mongodb')
 
+const connectionInstance = null
 async function connectToDatabase() {
+  if (connectionInstance) return connectionInstance
   const client = new MongoClient(process.env.MONGODB_CONNECTIONSTRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -21,7 +24,47 @@ function extractBody(event) {
   return JSON.parse(event.body)
 }
 
+async function basicAuth(event) {
+  const { authorization } = event.headers
+  if (!authorization) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Missing authorization header' })
+    }
+  }
+
+  const [type, credentials] = authorization.split(' ')
+  if (type !== 'Basic') {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Invalid authorization type' })
+    }
+  }
+
+  const [username, password] = Buffer.from(credentials, 'base64').toString().split(':')
+  const hashedPass = pbkdf2Sync(password, process.env.SALT, 100000, 64, 'sha512').toString('hex')
+
+  const client = await connectToDatabase()
+  const collection = client.collection('users')
+  const user = await collection.findOne({ name: username, password: hashedPass })
+
+  if (!user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Invalid credentials' })
+    }
+  }
+
+  return {
+    id: user._id,
+    username: user.username
+  }
+}
+
 module.exports.sendResponse = async (event) => {
+  const authResult = await basicAuth(event)
+  if (authResult.statusCode === 401) return authResult
+
   const correctAnswers = [3, 1, 0, 2]
   const { name, answers } = extractBody(event)
   const totalCorrectAnswers = answers.reduce((acc, answer, index) => {
@@ -56,6 +99,9 @@ module.exports.sendResponse = async (event) => {
 }
 
 module.exports.getResult = async (event) => {
+  const authResult = await basicAuth(event)
+  if (authResult.statusCode === 401) return authResult
+
   const client = await connectToDatabase()
   const collection = client.collection('results')
   const result = await collection.findOne({ _id: new ObjectId(event.pathParameters.id) })
